38f017608133af55cd50c0c4f2dc3f87011d9d2b
Cypo <cypooriginal@gmail.com>
2022-10-23 01:13:18 +0200

    Upload files

--- topbutton.js
+++ topbutton.js
@@ -5,9 +5,13 @@ let mybutton = document.getElementById("top-button");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
     }
+}
+
+function scrollToTop(){
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
