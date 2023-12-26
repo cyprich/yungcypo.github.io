df6bf23865eccdb22991d49431f5f577f5c23a49
Peter Cyprich <cypooriginal@gmail.com>
2023-12-27 00:58:51 +0100

    started with header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import "/src/css/styles.css";
+import "/src/css/header.css";
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <header>
+            <span>
+                <Link to={"/"}>Domov</Link>
+                <Link to={"/#omne"}>O mne</Link>
+                <Link to={"/#projekty"}>Projekty</Link>
+                <Link to={"/#kontakt"}>Kontakt</Link>
+            </span>
+            <span>
+                <img id={"smile"} src="/src/images/smile.svg" alt=""/>
+            </span>
+        </header>
     );
 };
 
