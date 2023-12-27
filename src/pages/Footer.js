9cfb6dd02f22ac884b67e484007b2dfc5ea0ce44
Peter Cyprich <cypooriginal@gmail.com>
2023-12-27 01:14:37 +0100

    footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import "../css/styles.css"
+import "../css/footer.css"
 
 const Footer = () => {
     return (
-        <div>
-
-        </div>
+        <footer>
+            <div>
+                <p className={"light"}>Cypo's Website <span className={"lighter"}>v3</span></p>
+                <p className={"light"}>Created by Cypo</p>
+            </div>
+            <div>
+                <p className={"light"}>Zdrojový kód je sprístupnený na
+                    <a href="https://github.com/yungcypo/Website" target={"_blank"}> Github</a>
+                </p>
+                <p className={"light"}>2020 - 2024</p>
+            </div>
+        </footer>
     );
 };
 
