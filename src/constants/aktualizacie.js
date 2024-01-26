855ef4deefcce060c94d292498a4fd349232736a
Peter Cyprich <cypooriginal@gmail.com>
2024-01-26 19:23:29 +0100

    Create aktualizacie.js

---
+++ src/constants/aktualizacie.js
@@ -0,0 +1,12 @@
+const aktualizacie = [
+    {
+        datum: {
+            den: 0,
+            mesiac: 0,
+            rok: 0
+        },
+        popis: ""
+    }
+]
+
+export default aktualizacie
