d7fb0fd2da2fbb5bbea69ad83e5f85577a744b8c
Peter Cyprich <cypooriginal@gmail.com>
2024-01-29 13:40:24 +0100

    Update PageNotFound.jsx

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import "../css/styles.css"
-import "../css/404.css"
+import "../css/pagenotfound.css"
 import {Link} from "react-router-dom";
 
 const PageNotFound = () => {
     return (
         <div className={"pagenotfound projekt"}>
             <h1>404</h1>
-            <h4>Takúto stránku zatiaľ nemám, alebo je momentálne nedostupná</h4>
-            <h4>Ak problém pretrváva, <span style={{textDecoration: "underline"}}>kontaktuj ma</span></h4>
-            <Link to={"/"}><h4 className={"pagenotfoundlink"}>Choď domov</h4></Link>
+            <p>Takúto stránku zatiaľ nemám, alebo je momentálne nedostupná</p>
+            <p>Ak problém pretrváva, <span style={{textDecoration: "underline"}}>kontaktuj ma</span></p>
+            <Link to={"/"}><p className={"pagenotfoundlink"}>Choď domov</p></Link>
         </div>
     );
 };
