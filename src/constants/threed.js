b6a99854d8ddd716c89fcebc9e776246c740609b
Peter Cyprich <cypooriginal@gmail.com>
2024-01-31 01:07:17 +0100

    historia 3D + obrazky

---
+++ src/constants/threed.js
@@ -0,0 +1,85 @@
+const threed = [
+    {
+        podnadpis: "3D Pero",
+        datum: "December 2017",
+        popis : [
+            "Na vianoce roku 2017 som dostal 3D pero",
+            "3D pero fungovalo na podobnom princípe ako 3D tlačiareň",
+            "Veľkou nevýhodou však bola obtiažnosť tlače"
+        ],
+        obrazky : [
+            {
+                zdroj: require("../images/3dpero.png"),
+                popis: "3D pero"
+            }
+        ],
+        align: "left",
+    },
+    {
+        podnadpis: "3D tlač v škole",
+        datum: "Október 2023",
+        popis : [
+            "Na vysokej škole som si v prvom semestri prvého ročníka zapísal predmet 3D tlač",
+            "Vďaka tomuto predmetu a FriDLab-u som sa prvý krát dostal do kontaktu s 3D tlačiarňou",
+            "Taktiež som mal možnosť naučiť sa a pochopiť všetko potrebné ohľadom 3D tlače. Ďakujem!",
+            "Tlačiarne, s ktorými sme pracovali boli Prusa Mini+ a Prusa i3 MK3",
+        ],
+        obrazky : [
+            {
+                zdroj: require("../images/prusai3mk3s.jpg"),
+                popis: "Prusa i3 MK3S"
+            },
+            {
+                zdroj: require("../images/prusamini.jpg"),
+                popis: "Prusa Mini+"
+            },
+            {
+                zdroj: require("../images/fridlab.jpg"),
+                popis: "Logo FriDLab"
+            },
+            {
+                zdroj: require("../images/prvyvytlacok.jpg"),
+                popis: "Môj prvý výtlačok"
+            },
+        ],
+        align: "left",
+    },
+    {
+        podnadpis: "Prvá tlačiareň?",
+        datum: "20. November 2023",
+        popis : [
+            "Pre ostatných bol tento deň len normálny pondelok, no pre mňa to bol deň, kedy som si objednal svoju prvú 3D tlačiareň",
+            "Po dlhom uvažovaní, rozhodovaní a pozeraní recenzií som sa rozhodol kúpiť si tlačiareň Bambu Lab A1 mini v predpredaji",
+            "Očakávaný dátum dodania bol vzhľadom na predpredaj až v januári roku 2024",
+            "Neostávalo mi nič iné než pustiť sa do prudkého čakania, ktoré miestami sa zdalo byť nekonečné"
+        ],
+        obrazky : [
+            {
+                zdroj: require("../images/bambulab.png"),
+                popis: "Logo Bambu Lab"
+            },
+        ],
+        align: "left",
+    },
+    {
+        podnadpis: "Prvá tlačiareň!",
+        datum: "28. December 2024",
+        popis : [
+            "Po nekonečne dlhom čakaní o dĺžke 38 dní som konečne dostal oznámenie o doručení svojej nekonečne dlho očakávanej zásielky",
+            "Hneď, ako bolo možné som tlačiareň rozbalil, spojazdnil a vytlačil svoj prvý výtlačok - 3D Benchy"
+        ],
+        obrazky : [
+            {
+                zdroj: require("../images/a1mini.jpg"),
+                popis: "Bambu Lab A1 mini"
+            },
+            {
+                zdroj: require("../images/benchy.jpg"),
+                popis: "Prvý výtlačok mojej tlačiarne"
+            },
+        ],
+        align: "left",
+    },
+]
+
+export default threed;
--- src/pages/ThreeD.jsx
+++ src/pages/ThreeD.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import "../css/styles.css"
 import "../css/threed.css"
 
+import CasovaOs from "./CasovaOs";
+
+import threed from "../constants/threed";
+
 const ThreeD = () => {
     return (
         <div className={"threed projekt"}>
             <h2>3D tlač</h2>
             <h5 style={{display: "flex", gap: "0.5em"}}>
-                popis
+
             </h5>
+            <div className={"threedcasovaos"}>
+                <h3 className={"casovaOsNadpis"}>Ako som sa dostal ku 3D tlači</h3>
+                <CasovaOs file={threed}/>
+            </div>
         </div>
     );
 };
