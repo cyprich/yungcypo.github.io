d6d86fb075511e3167bbfdc9440079e6161bac75
Peter Cyprich <cypooriginal@gmail.com>
2024-02-01 11:52:06 +0100

    zacate zatazenie

---
+++ src/constants/zatazenie.js
@@ -0,0 +1,37 @@
+const zatazenie = {
+    hodnotyHeader: [
+        {
+            nazov: "Materiál"
+        },
+        {
+            nazov: "Hrúbka",
+            jednotka: `$[m]$`
+        },
+        {
+            nazov: "Objemová tiaž",
+            jednotka: `$[\\frac{KN}{m^3}]$`
+        },
+        {
+            nazov: "Zaťažovacia plocha",
+            jednotka: `$[m^2]$`
+        },
+        {
+            nazov: "Charakteristická hodnota zaťaženia",
+            znacka: "$f_k$",
+            jednotka: "$[\\frac{KN}{m^2}]$"
+        },
+        {
+            nazov: "Súčiniteľ zaťaženia",
+            znacka: "$\\gamma_F$",
+        },
+        {
+            nazov: "Návrhová hodnota zaťaženia",
+            znacka: "$f_d$",
+            jednotka: "$[\\frac{KN}{m^2}]$"
+        }
+    ],
+    konstanty: []
+}
+
+
+export default zatazenie
--- src/pages/Zatazenie.jsx
+++ src/pages/Zatazenie.jsx
@@ -1,15 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import "../css/styles.css"
 import "../css/zatazenie.css"
+import Latex from "react-latex";
+
+import zatazenie from "../constants/zatazenie";
 
 const Zatazenie = () => {
+    var Latex = require("react-latex");
+
     const [strecha, setStrecha] = useState(false);
+    const [vysledky, setVysledky] = useState([]);
 
-    const hodnotyStrop = ["Škola, Materská škola, Administratívna budova, Knižnica, Reštaurácia, Divadelná sála, Predajňa, Nemocnica".split(", ")]
-    const hodnotyStrecha = ["I., II., III., IV.".split(", ")]
+    const hodnotyStrop = ["Škola", "Materská škola", "Administratívna budova", "Knižnica", "Reštaurácia", "Divadelná sála", "Predajňa", "Nemocnica"]
+    const hodnotyStrecha = ["I.", "II.", "III.", "IV."]
 
-    console.log(hodnotyStrop)
-    console.log(hodnotyStrecha)
 
     /* scroll to top */
     useEffect(() => {
@@ -34,9 +38,55 @@ const Zatazenie = () => {
                                     : require("../images/icons/strop.png")
                             } alt=""/>
                     </button>
+                    <select name="select">
+                        <option value={null}>Vyberte {strecha ? "snehovú oblasť" : "typ prevádzky"}</option>
+                        <optgroup>
+                            {
+                                strecha
+                                    ? hodnotyStrecha.map((e, key) => {
+                                        return (
+                                            <option value={key} key={key}>{e}</option>
+                                        )
+                                    })
+                                    : hodnotyStrop.map((e, key) => {
+                                        return (
+                                            <option value={key} key={key}>{e}</option>
+                                        )
+                                    })
+                            }
+                        </optgroup>
+                    </select>
+                </div>
+            </div>
+            <div className="zatazenievysledky">
+                <div className="zatazenieheader">
                     {
-                        
+                        zatazenie.hodnotyHeader.map((e, key) => {
+                            return (
+                                <div className="zatazenieheaderhodnota" key={key}>
+                                    <div className="nazov">{e.nazov}</div>
+                                    <div>
+                                        {
+                                            e.znacka
+                                                ? <div className="znacka"><Latex>{e.znacka}</Latex></div>
+                                                : null
+                                        }
+                                        {
+                                            e.jednotka
+                                                ? <div className="jednotka"><Latex>{e?.jednotka}</Latex></div>
+                                                : null
+                                        }
+                                    </div>
+                                </div>
+                            )
+                        })
                     }
+                </div>
+                <div className="zatazeniebody">
+
+                </div>
+                <div className="zatazeniefooter">
+
                 </div>
             </div>
         </div>
