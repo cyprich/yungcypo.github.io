f42dfcfcf972884232166eecf0a28710d0c18141
Peter Cyprich <cypooriginal@gmail.com>
2024-02-02 16:57:06 +0100

    zatazenie done

--- src/pages/Zatazenie.jsx
+++ src/pages/Zatazenie.jsx
@@ -24,13 +24,19 @@ const Zatazenie = () => {
     const [objemovaTiazInputHodnota, setObjemovaTiazInputHodnota] = useState("");
 
     const [gk, setGk] = useState(null);
-    const [qk, setQk] = useState(1);
+    const [qk, setQk] = useState(null);
     const [fck, setFck] = useState(null);
     const [gammaf1, setGammaf1] = useState(1.35);
     const [gammaf2, setGammaf2] = useState(1.50);
     const [gd, setGd] = useState(null);
     const [qd, setQd] = useState(null);
     const [fcd, setFcd] = useState(null);
+
+    const [sk, setSk] = useState(null);
+    const [mii, setMii] = useState(0.8);
+    const [ce, setCe] = useState(1);
+    const [ct, setCt] = useState(1);
+    const [so, setSo] = useState(null);
 
     const pridajRiadok = () => {
         let novyVysledok
@@ -68,28 +74,75 @@ const Zatazenie = () => {
     }
 
     useEffect(() => {
-        // TODO setQk podla selectu - z tabuliek
-        if (strecha) {
-
-        } else {
-
-        }
-    }, [selectValue]);
-
-
-    useEffect(() => {
         let novegk = 0
         vysledky.map((e) => {
             if (e.objemovaTiaz != "-") {
                 novegk += e.charakteristickaHodnotaZatazenia
             }
         })
-        setGk(novegk)
-        setFck(gk * qk)
-        setGd(gk * gammaf1)
-        setQd(qk * gammaf2)
-        setFcd(gd + qd)
-    }, [vysledky]);
+        setGk(Math.round((novegk) * 1000) / 1000)
+
+        if (strecha) {
+            switch (Number(selectValue)) {
+                case 0:
+                    setSo(0.75)
+                    break
+                case 1:
+                    setSo(1.05)
+                    break
+                case 2:
+                    setSo(1.5)
+                    break
+                case 3:
+                    setSo(2.25)
+                    break
+            }
+        } else if (!strecha && selectValue != null) {
+            switch (Number(selectValue)) {
+                case 0:
+                case 1:
+                case 2:
+                case 4:
+                case 6:
+                case 7:
+                    setQk(3)
+                    break
+                case 3:
+                    // kniznica
+                    setQk(7.5)
+                    break
+                case 5:
+                    // divadelna sala
+                    setQk(4)
+                    break
+
+            }
+        }
+        setSk(Math.round((mii * ce * ct * so) * 100) / 100)
+
+        if (strecha) {
+            if (sk && gk) {
+                setFck(Math.round((sk * gk) * 1000) / 1000)
+            }
+            if (sk && gammaf2) {
+                setQd(Math.round((sk * gammaf2) * 1000) / 1000)
+            }
+        } else {
+            if (gk && qk) {
+                setFck(Math.round((gk * qk) * 1000) / 1000)
+            }
+            if (qk && gammaf2) {
+                setQd(Math.round((qk * gammaf2) * 1000) / 1000)
+            }
+        }
+
+        if (gk && gammaf1) {
+            setGd(Math.round((gk * gammaf1) * 1000) / 1000)
+        }
+        if (gd && gd) {
+            setFcd(Math.round((gd + qd) * 1000) / 1000)
+        }
+    }, [vysledky, selectValue, gk, so, qk, sk, fck, gd, qd, fcd]);
 
 
     /* scroll to top */
@@ -101,11 +154,11 @@ const Zatazenie = () => {
         <div className={"zatazenie projekt"}>
             <div className={"zatazenienadpisy"}>
                 <h2>Zaťaženie</h2>
-                <h4>Výpočet zaťaženia na {strecha ? "strechu" : "strop"}</h4>
+                <h4>Výpočet zaťaženia na {strecha ? "plochú strechu" : "strop"}</h4>
             </div>
             <div className="zatazenieinputy">
                 <div>
-                    <button title={"Zmeniť strop/strechax"} onClick={() => {
+                    <button title={"Zmeniť strop / strecha"} onClick={() => {
                         setStrecha(!strecha)
                     }}>
                         <img
@@ -143,7 +196,7 @@ const Zatazenie = () => {
                     <tr>
                         {zatazenie.hodnotyHeader.map((e, key) => {
                             return (
-                                <td colSpan={key >= 4 ? 2 : 1}>
+                                <td colSpan={key >= 4 ? 2 : 1} key={key}>
                                     <div className={"nazov"}>{e.nazov}</div>
                                     <div className={"vyrazne"}><Latex>{e?.znacka}</Latex></div>
                                     <div className={"nevyrazne"}><Latex>{e?.jednotka}</Latex></div>
@@ -155,7 +208,7 @@ const Zatazenie = () => {
                     <tbody>
                     {vysledky.map((e, key) => {
                         return (
-                            <tr>
+                            <tr key={key}>
                                 <td>{e.material}</td>
                                 <td>{e.hrubka}</td>
                                 <td>{e.objemovaTiaz}</td>
@@ -224,7 +277,11 @@ const Zatazenie = () => {
                             }
                         </td>
                         <td className={"vyrazne malicke"}>{strecha ? "S" : "q"}<sub>k</sub></td>
-                        <td>{qk ? qk : null}</td>
+                        {
+                            strecha
+                                ? <td>{sk ? sk : null}</td>
+                                : <td>{qk ? qk : null}</td>
+                        }
                         <td colSpan={2}>{gammaf2}</td>
                         <td className={"vyrazne malicke"}>{strecha ? "S" : "q"}<sub>d</sub></td>
                         <td>{qd ? qd : null}</td>
@@ -243,6 +300,17 @@ const Zatazenie = () => {
                     </tr>
                     </tfoot>
                 </table>
+                {
+                    (strecha && sk)
+                        ? <div className="afterfooter">
+                            <div><Latex>{`$S_K = \\mu_i \\times c_e \\times c_t \\times S_O $`}</Latex></div>
+                            <div>
+                                <Latex>{`$S_K = ` + mii + ` \\times ` + ce + ` \\times ` + ct + ` \\times ` + so + `$`}</Latex>
+                            </div>
+                            <div><Latex>{`$S_K = ` + sk + ` $`}</Latex></div>
+                        </div>
+                        : null
+                }
             </div>
         </div>
     );
