5484ee03e706192cfe1ee58d7adc6087ac0f854e
Peter Cyprich <cypooriginal@gmail.com>
2024-02-02 20:23:08 +0100

    nova mudrost

--- src/constants/mudrosti.js
+++ src/constants/mudrosti.js
@@ -1,6 +1,10 @@
 const mudrosti = [
     "Kto druhému zaseje, tak za z hory ozýva",
-    "Nie som urológ, aby som sa s ko🤬🤪tom zaoberal",
+    /*
+    "Nie som urológ, aby som sa s ko🤬otom zaoberal",
+    "Nie som urológ, aby som sa s ko🤪otom zaoberal",
+    */
+    "Dole ruky, hore gate, máte tam čo ukáž",
 ]
 
 export default mudrosti
