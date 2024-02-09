cb62724569191d96cb1f67c33c6c6985b57dce39
Peter Cyprich <cypooriginal@gmail.com>
2024-02-10 00:08:35 +0100

    pridane mudrosti

--- src/constants/mudrosti.js
+++ src/constants/mudrosti.js
@@ -1,10 +1,14 @@
 const mudrosti = [
     "Kto druhému zaseje, tak za z hory ozýva",
+    "Dole ruky, hore gate, máte tam čo ukáž",
+    "Ako sa do lesa volá, sám do nej padá",
+    "Nesúď knihu ďaleko od stromu",
+    "Nehovor hop, kým kopeš jamu",
+    //"Lepšie hory prenášať, ako",
     /*
     "Nie som urológ, aby som sa s ko🤬otom zaoberal",
     "Nie som urológ, aby som sa s ko🤪otom zaoberal",
     */
-    "Dole ruky, hore gate, máte tam čo ukáž",
 ]
 
 export default mudrosti
