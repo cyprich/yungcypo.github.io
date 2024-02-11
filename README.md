6bb83ccb818c811bf27ea13eb0a534d3934eb50b
Peter Cyprich <cypooriginal@gmail.com>
2024-02-11 22:00:26 +0100

    berem spat ten deploy lebo tragedia akoze

---
+++ README.md
@@ -0,0 +1,5 @@
+# Website
+[![wakatime](https://wakatime.com/badge/user/4c514061-8f41-4da2-97ea-f2b4906774a3/project/5c9299a1-55c3-4c83-be4d-4ec507d36b91.svg)](https://wakatime.com/badge/user/4c514061-8f41-4da2-97ea-f2b4906774a3/project/5c9299a1-55c3-4c83-be4d-4ec507d36b91)  
+
+[Link na moju stránku](https://yungcypo.github.io)  
+Momentálne je vo vývoji [stránka v3](https://github.com/yungcypo/yungcypo.github.io/tree/react)
---
+++ assets/js/edabit.js
@@ -0,0 +1,34 @@
+/* Very easy*/
+function addition1(a, b){console.log(a + b)}
+function convert1(a){console.log(a * 60)}
+function addition2(a){console.log(a++)}
+function triArea(a, va){console.log((a * va) / 2)}
+function calcAge(a){console.log(a * 365)}
+
+/* Easy */
+function addUp(x){
+    let result = 0
+    for(x; x > 0; x--){
+        result += x
+    }
+    console.log(result)
+}
+function matchHouses(x){console.log(
+    1 + x * 5
+)}
+function shiftToLeft(x, y){console.log(
+    x * Math.pow(2, y)
+)}
+
+
+
+
+
+
+
+
+
+
+
+
+
