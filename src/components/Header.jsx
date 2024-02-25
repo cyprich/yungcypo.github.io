36303ee7444ad876ec8b95af88f6f7401b802d1b
Peter Cyprich <cypooriginal@gmail.com>
2024-02-26 00:20:14 +0100

    zacaty header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <nav className={"flex h-16 w-full bg-black fixed top-0 left-0 z-50 text-gray-50 "}>
-            <p>Header</p>
+        <nav className={"flex justify-between items-center h-16 px-8 w-full bg-black fixed top-0 left-0 z-50 text-gray-50 "}>
+            <ul className={"flex gap-12 select-none cursor-pointer"}>
+                <li className={"hover:text-red-900"}>Domov</li>
+                <li className={"hover:text-red-900"}>Projekty</li>
+                <li className={"hover:text-red-900"}>Kontakt</li>
+            </ul>
+            <img className={"w-10 h-10"} src={require("../images/icons/smile.png")} alt=""/>
         </nav>
     );
 };
