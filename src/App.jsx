2b13a431d0b5691731272cf51cead768d890dfce
Peter Cyprich <cypooriginal@gmail.com>
2024-02-25 18:41:29 +0100

    total startover

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-import Home from "./pages/Home";
-import Header from "./components/Header";
-import Footer from "./components/Footer"
-import FooterPhone from "./components/FooterPhone";
-import PageNotFound from "./pages/PageNotFound";
-import Updates from "./pages/Updates"
-import Kontakt from "./components/Kontakt";
-import Disclaimer from "./pages/Disclaimer";
-import Popup from "./pages/Popup";
-import Login from "./pages/Login";
-import Profil from "./pages/Profil"
-
-import Stavitelstvo from "./pages/Stavitelstvo";
-import Schodisko from "./pages/Schodisko";
-import Zatazenie from "./pages/Zatazenie";
-import Vystuz from "./pages/Vystuz";
-
-import ThreeD from "./pages/ThreeD";
-import ThreeDHistoria from "./pages/ThreeDHistoria";
-import ThreeDKalkulacka from "./pages/ThreeDKalkulacka";
-import ThreeDModely from "./pages/ThreeDModely";
-import ThreeDFilamenty from "./pages/ThreeDFilamenty";
-import ThreeDFilamentyNovy from "./pages/ThreedFilamentyNovy";
-import ThreedPrusameter from "./pages/ThreedPrusameter";
-
-import Odpisy from "./pages/Odpisy";
-import KvadratickeRovnice from "./pages/KvadratickeRovnice";
-import NalepkyNaFilament from "./pages/NalepkyNaFilament";
-
 const App = () => {
     return (
         <>
             <Router>
-                <Header/>
-                <Popup/>
                 <Routes>
-                    <Route path={"/"} element={<Home/>}/>
-                    <Route path={"/updates"} element={<Updates/>}></Route>
-                    <Route path={"/kontakt"} element={<Kontakt className={"projekt"}/>}></Route>
-                    <Route path={"/disclaimer"} element={<Disclaimer/>}></Route>
-                    <Route path={"/vyhlasenieovylucenizodpovednosti"} element={<Disclaimer/>}></Route>
-                    <Route path={"/login"} element={<Login/>}></Route>
-                    <Route path={"/prihlasenie"} element={<Login/>}></Route>
-                    <Route path={"/profil"} element={<Profil/>}></Route>
-
-                    <Route path={"/stavitelstvo"} element={<Stavitelstvo/>}></Route>
-                    <Route path={"/schodisko"} element={<Schodisko/>}/>
-                    <Route path={"/zatazenie"} element={<Zatazenie/>}/>
-                    <Route path={"/vystuz"} element={<Vystuz/>}/>
-
-                    <Route path={"/3D"} element={<ThreeD/>}/>
-                    <Route path={"/3D/historia"} element={<ThreeDHistoria/>}/>
-                    <Route path={"/3D/kalkulacka"} element={<ThreeDKalkulacka/>}/>
-                    <Route path={"/3D/modely"} element={<ThreeDModely/>}/>
-                    <Route path={"/3D/filamenty"} element={<ThreeDFilamenty/>}/>
-                    <Route path={"/3D/filamenty/novy"} element={<ThreeDFilamentyNovy/>}/>
-                    <Route path={"/3D/prusameter"} element={<ThreedPrusameter/>}/>
-                    <Route path={"/3D/nalepkynafilament"} element={<NalepkyNaFilament/>}/>
-
-                    <Route path={"/odpisy"} element={<Odpisy/>}/>
-                    <Route path={"/kvadratickerovnice"} element={<KvadratickeRovnice/>}/>
-
-                    <Route path={"*"} element={<PageNotFound/>}/>
+                    <Route path={"*"} element={<p>404</p>}/>
                 </Routes>
-                <Footer/>
-                <FooterPhone/>
             </Router>
         </>
     );
