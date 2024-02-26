fbce554c61d49a7f858aac1ccb474270f3157160
Peter Cyprich <cypooriginal@gmail.com>
2024-02-26 16:43:26 +0100

    omne vlnky

--- src/components/home/Omne.jsx
+++ src/components/home/Omne.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import OmneItem from "./OmneItem";
 
+import {ReactComponent as Wave1} from "../../images/wave1.2.svg";
+import {ReactComponent as Wave2} from "../../images/wave2.2.svg";
+
 const Omne = () => {
     return (
-        <ul className={"grid grid-cols-3 px-48 py-12 gap-16"}>
-            <OmneItem text={"Prievidza"} src={require("../../images/3dicons/house.png")}/>
-            <OmneItem text={"4. 6. 2003"} src={require("../../images/3dicons/calendar.png")}/>
-            <OmneItem text={"FRI UNIZA"} src={require("../../images/3dicons/computer.png")}/>
-        </ul>
+        <>
+            <ul className={"relative grid grid-cols-3 px-48 py-16 gap-16 bg-red-100"}>
+                <Wave1 class={"absolute top-[-32px]"}/>
+                <OmneItem text={"Prievidza"} src={require("../../images/3dicons/house.png")}/>
+                <OmneItem text={"4. 6. 2003"} src={require("../../images/3dicons/calendar.png")}/>
+                <OmneItem text={"FRI UNIZA"} src={require("../../images/3dicons/computer.png")}/>
+                <Wave2 class={"absolute bottom-[-32px]"}/>
+            </ul>
+        </>
     );
 };
 
