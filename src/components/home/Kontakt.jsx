ed833ccca67fa8fc5e98e761bac95a7e5b004271
Peter Cyprich <cypooriginal@gmail.com>
2024-03-01 19:09:49 +0100

    kontakt dispozition

--- src/components/home/Kontakt.jsx
+++ src/components/home/Kontakt.jsx
@@ -6,9 +6,11 @@ const Kontakt = () => {
         <section className={"flex justify-between items-center px-36 py-16"}>
             <div className={"flex flex-col gap-3 border border-gray-200 rounded-2xl p-8 w-1/2"}>
                 <p className={"text-3xl font-bold"}>Kontakt</p>
-                <input type="text" placeholder={"Meno"}/>
-                <input type="text" placeholder={"E-mail"}/>
-                <TextareaAutosize className={"customscrollbar"} minRows={3} maxRows={10} placeholder={"Správa"}/>
+                <div className={"flex gap-3"}>
+                    <input className={"w-2/3"} type="text" placeholder={"Meno"}/>
+                    <input className={"w-full"} type="text" placeholder={"E-mail"}/>
+                </div>
+                <TextareaAutosize className={"customscrollbar"} minRows={4} maxRows={10} placeholder={"Správa"}/>
                 <button className={"button button-red rounded-xl"}>Odoslať</button>
             </div>
             <img className={"h-96 drop-shadow-xl"} src={require("../../images/3danimals/medvedikcistotny.png")} alt=""/>
