4a9884f85515ca7a66c0e8f180fb7d4200a0af12
Peter Cyprich <cypooriginal@gmail.com>
2024-03-01 21:04:35 +0100

    omne bit fix

--- src/components/home/OmneItem.jsx
+++ src/components/home/OmneItem.jsx
@@ -4,7 +4,7 @@ const OmneItem = ({text, src}) => {
     return (
         <li className={"relative w-full"}>
             <div className={"p-8 text-center bg-gradient-to-tr from-red-400 to-red-600 rounded-2xl"}>
-                <img className={"p-6 drop-shadow-2xl"} src={src} alt=""/>
+                <img className={"drop-shadow-2xl"} src={src} alt=""/>
                 <p className={"font-bold text-2xl mt-2 drop-shadow-xl"}>{text}</p>
             </div>
         </li>
